f362fe9676a0a53702578d10f3a555246ac65272
BCARE-2022 <107754697+BCARE-2022@users.noreply.github.com>
2022-06-18 14:39:27 +0300

    Add files via upload

---
+++ bcare/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="ar" dir='rtl'>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../css/all.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Changa:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <title>BCARE</title>
+</head>
+
+<body>
+
+    <!-- //////////////////////////////////////// -->
+    <div class="home">
+        <img class="mt-2" src="../img/bcare-logo.svg" alt="" width="50px" height="50px">
+        <img src="../img/carbg2.jpg" alt="">
+        <a href="https://wa.me/message/BA7QB34RTGRYA1" target="_blank">
+            <div class="bg-warning cardcar ">
+                <img class=" mb-3" src="../img/002-02.svg" alt="" width="30px" height="30px" />
+                <h4>تأمين المركبات</h4>
+            </div>
+        </a>
+
+        <!-- ///////////////////////////////////////////////// -->
+        <div class="blog  ">
+            <div class="container  wow animate__fadeInUp">
+                <div class="main-title mt-5position-relative text-center">
+                    <h2 class="mt-4">ليش بي كير خيارك الأول في التأمين ؟</h2>
+                    <p class="text-black-50 text-uppercase">مالك في الطويلة ..عشان ماتتعب نفسك وتدور أفضل و “أوضح” عرض بدون
+                        ماتحتار</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- //////////////////////////////////////////////////////// -->
+        <div id="Abute" class="Stuff  mb-1 ">
+            <div class="container  wow animate__fadeInUp">
+                <div class="col-lg-5">
+                    <img class="img-fluid" src="../img/WhatsApp Image 2022-06-03 at 12.59.02 AM.jpeg" alt="" />
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-lg-5 text-center text-md-start">
+                        <div class="text mb-5">
+                            <h3 class="mb-3">أهلاً بك معنا في الآنطلاقة الجديدة لشركة "عناية الوسيط بي كير".
+                            </h3>
+                            <p class="text-black-50">
+                                نحن اليوم نبدأ تميزنا في مجال وساطة التأمين وإعادة التأمين، ولا ينحصر هذا التميز على تقديم التغطيات التأمينية، فحسب؛ بل يمتد ليظهر إتجاهاً جديداً في العناية بالعملاء، فنحن نسعى دوماً للرقي بعلاقتنا مع العملاء وتقديم الاستشارات والخدمات التأمينية بطريقة مبتكرة ومختلفة، تجعل التغطيات التأمينية وإدارة الخطر أمراً سهلاً، وواضحاً لعملائنا الكرام.
+                            </p>
+                            <p class="text-black-50">
+                                تدرك شركة "عناية الوسيط بي كير" أن الهدف من التأمين هو توفير الحماية للأفراد، والمؤسسات، والشركات، ومن هذا المنطلق تم انتقاء أفضل الكوادر البشرية، وتوظيف أفضل التقنيات؛ لتوفير هذه الحماية بجودة عالية تراعي أدق تفاصيل احتياجات العملاء؛ لتحقيق تطلعاتهم وآمالهم.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- ///////////////////////////////////////////////// -->
+        <a href="https://wa.me/message/BA7QB34RTGRYA1" target="_blank">
+            <div class="bg-warning cardcar mb-5 ">
+                <h4>تقديم الطلب</h4>
+            </div>
+        </a>
+        <!-- //////////////////////////////////////////////////////// -->
+        <div class="blog pt-0 p-4">
+            <h2 class="text-center mb-3">أهداف بي كير
+            </h2>
+            <div class="col-lg-4 col-md-4 col-sm-4 mb-5">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="text-black-50">لمساهمة في تحقيق قيمة مـضـافـة لـسوق التـأمين من خــــلال تـقـنـيـة الـمـعـلـومـات والـذكـاء الاصـطـنـاعـــي.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 mb-5">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="text-black-50">تكثيف مَجهودات الشركـة لتقديم خدمات تتفق مــع احتياجات ورغبات العملاء.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 mb-3">
+                <div class="card">
+                    <div class="card-body">
+                        <p class="text-black-50">زيادة الوعي التأميني لـلعـمـلاء وحـمـايـتـهم من جـمـيـع الــمـخـاطــر الـتـي تـحـيـط بـهــم.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- //////////////////////////////////////////////////// -->
+        <a href="https://wa.me/message/BA7QB34RTGRYA1" target="_blank">
+            <div class="bg-warning cardcar mb-5 ">
+                <h4>تقديم الطلب</h4>
+            </div>
+        </a>
+
+        <!-- ///////////////////////////////////////////////// -->
+        <div class="combany mt-3 col-lg-2 mb-3 col-sm-6 ">
+            <h2>شركاؤنا</h2>
+        </div>
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active sliderr">
+                    <img src="../img/ACIG.svg" alt="" />
+                    <img src="../img/AICC.svg" alt="" />
+                    <img src="../img/Alalamiya.svg" alt="" />
+                    <img src="../img/Buruj.svg" alt="" />
+                    <img src="../img/ArabianShield.svg" alt="" />
+                    <img src="../img/AlRajhi.svg" alt="" />
+                </div>
+                <div class="carousel-item sliderr">
+                    <img src="../img/UCA.svg" alt="" />
+                    <img src="../img/Salama.svg" alt="" />
+                    <img src="../img/Walaa.svg" alt="" />
+                    <img src="../img/Wataniya.svg" alt="" />
+                    <img src="../img/TUIC.svg" alt="" />
+                    <img src="../img/TokioMarine.svg" alt="" />
+                </div>
+                <div class="carousel-item sliderr">
+                    <img src="../img/Tawuniya.svg" alt="" />
+                    <img src="../img/SaudiEnaya.svg" alt="" />
+                    <img src="../img/Sagr.svg" alt="" />
+                    <img src="../img/MedGulf.svg" alt="" />
+                    <img src="../img/Malath.svg" alt="" />
+                    <img src="../img/GGI.svg" alt="" />
+                </div>
+                <div class="carousel-item sliderr">
+                    <img src="../img/AXA.svg" alt="" />
+                    <img src="../img/Allianz.svg" alt="" />
+                    <img src="../img/GulfUnion.svg" alt="" />
+                    <img src="../img/TUIC.svg" alt="" />
+                    <img src="../img/Buruj.svg" alt="" />
+                    <img src="../img/SAICO.svg" alt="" />
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+        <!-- ///////////////////////////// -->
+
+        <!-- ///////////////////////////////////////////////// -->
+        <footer id="Contact" class=" foot pb-3  pt-5 text-center  ">
+            <div>
+                <div class=" d-flex align-items-center">
+                    <span>مصرح من قبل :</span>
+                    <img class=" mb-3" src="../img/sama-logo.svg" alt="" width="100px" height="100px">
+                </div>
+                <div>
+                    <span>966595151158+</span>
+                    <i class="fa-brands fa-whatsapp p-2"></i>
+                    <span>966595160052+</span>
+                    <i class="fa-solid fa-mobile-screen-button p-2"></i>
+                </div>
+
+            </div>
+            <div class="textfooter p-3">
+                <p>الشروط والأحكام | سياسة الخصوصية | قواعد ولوائح البنك المركزي السعودي | عن الشركة | اطبع وثيقتك
+                    راسلنا | المدونة | خصومات وريف | التوظيف | طريقة رفع شكوى لساما |</p>
+                <p>جميع الحقوق محفوظة , شركة عناية الوسيط لوساطة التأمين <b>© 2022 </b></p>
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
